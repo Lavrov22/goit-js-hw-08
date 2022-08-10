cd2b0a481ddded8417330e9b828b53c8a0902f1a
Lavrov Andrey <Sattarlav@gmail.com>
2022-08-10 23:21:36 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+ const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+let currentVideoTime;
+const STORAGE_KEY = "videoplayer-current-time";
+
+    const onPlay = function(data) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+    };
+currentVideoTime = localStorage.getItem(STORAGE_KEY);
+const videoSeconds = JSON.parse(currentVideoTime).seconds;
+if (currentVideoTime) {
+    player.setCurrentTime(videoSeconds);
+}
+
+    player.on('timeupdate', throttle(onPlay, 1000));
+
+   
+
 
