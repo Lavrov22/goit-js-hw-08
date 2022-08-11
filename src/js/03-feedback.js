b48532c43bd6c4fd4a3f5630ec2aad510a3036d7
Lavrov Andrey <Sattarlav@gmail.com>
2022-08-11 20:48:51 +0300

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = "feedback-form-state";
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('textarea'),
+    input: document.querySelector('input'),
+}
+let formData = {};
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+fillForm();
+function onFormInput(e){
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    console.log(formData);
+}
+
+function fillForm() {
+   const  savedLocalStorage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedLocalStorage) {
+        refs.input.value = savedLocalStorage.email || '';
+        refs.textarea.value = savedLocalStorage.message || '';
+    }
+}
+
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    localStorage.removeItem(STORAGE_KEY);
+    e.target.reset();
+    
+}
