fe08c949212dc33bee65aca291d403733b3994d3
Lavrov Andrey <Sattarlav@gmail.com>
2022-08-11 10:30:51 +0300

    update mentor task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,14 +10,15 @@ const STORAGE_KEY = "videoplayer-current-time";
 const onPlay = function (data) {
     console.log(data);
         localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
-    };
-currentVideoTime = localStorage.getItem(STORAGE_KEY);
-const videoSeconds = JSON.parse(currentVideoTime).seconds;
-if (currentVideoTime) {
-    player.setCurrentTime(videoSeconds);
-}
+};
+ player.on('timeupdate', throttle(onPlay, 1000));
 
-    player.on('timeupdate', throttle(onPlay, 1000));
+currentVideoTime = localStorage.getItem(STORAGE_KEY);
+let videoSeconds = JSON.parse(currentVideoTime);
+if (currentVideoTime) {
+    player.setCurrentTime(videoSeconds.seconds);
+} else{player.setCurrentTime(0)}
+
 
    
 
